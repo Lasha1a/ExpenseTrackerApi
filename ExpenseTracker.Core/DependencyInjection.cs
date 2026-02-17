2ad2d04ecf47d1ecb012897388bee33927f1915e
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-02-17 11:38:19 +0400

    initial commit

---
+++ ExpenseTracker.Core/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Core;
+
+public static class DependencyInjection
+{
+
+}
