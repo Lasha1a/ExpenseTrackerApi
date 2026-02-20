429c2676757ae632a3d35d3102a463ba7c340fdf
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-02-20 10:47:52 +0400

    added ExpenseResponse for Get/List responses

---
+++ ExpenseTracker.Application/Expenses/DTOs/ExpenseResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Application.Expenses.DTOs;
+
+internal class ExpenseResponse //for get/list responses
+{
+    public Guid Id { get; init; }
+
+    public Guid UserId { get; init; }
+    public Guid CategoryId { get; init; }
+
+    public decimal Amount { get; init; }
+    public string? Description { get; init; }
+    public DateTime ExpenseDate { get; init; }
+
+    public DateTime CreatedAt { get; init; }
+    public DateTime? UpdatedAt { get; init; }
+}
--- ExpenseTracker.Application/obj/Debug/net9.0/ExpenseTracker.Application.AssemblyInfo.cs
+++ ExpenseTracker.Application/obj/Debug/net9.0/ExpenseTracker.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02d493912e016496883a175b1ca684f6cf88618d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7aac76dc180e8fe141e8bb591c9a99ea842645a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
