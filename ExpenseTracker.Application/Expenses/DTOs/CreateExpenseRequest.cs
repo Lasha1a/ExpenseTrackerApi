02d493912e016496883a175b1ca684f6cf88618d
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-02-20 10:34:08 +0400

    added createexpenseDto

---
+++ ExpenseTracker.Application/Expenses/DTOs/CreateExpenseRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Application.Expenses.DTOs;
+
+internal class CreateExpenseRequest
+{
+    public Guid UserId { get; init; }
+    public Guid CategoryId { get; init; }
+
+    public decimal Amount { get; init; }
+    public string? Description { get; init; }
+    public DateTime ExpenseDate { get; init; }
+
+}
