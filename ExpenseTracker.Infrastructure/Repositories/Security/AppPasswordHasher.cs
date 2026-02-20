d2cae4ebc9107d44ffda3ddd91ad436a3d942c15
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-02-20 13:37:48 +0400

    added password hasher interface and implementation

---
+++ ExpenseTracker.Infrastructure/Repositories/Security/AppPasswordHasher.cs
@@ -0,0 +1,27 @@
+﻿using ExpenseTracker.Application.Interfaces.Security;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Infrastructure.Repositories.Security;
+
+public class AppPasswordHasher : IPasswordHasher
+{
+    private readonly PasswordHasher<object> _hasher = new(); // We can use the built-in PasswordHasher from ASP.NET Core Identity, which provides a secure way to hash passwords.
+
+    public string Hash(string password) // Hash the password using the PasswordHasher
+    {
+        return _hasher.HashPassword(null!, password);
+    }
+
+    public bool Verify(string password, string passwordHash) // Verify the password against the stored hash
+    {
+        var result = _hasher.VerifyHashedPassword(null!, passwordHash, password);
+
+        return result == PasswordVerificationResult.Success;
+    }
+}
--- ExpenseTracker.Infrastructure/obj/Debug/net9.0/ExpenseTracker.Infrastructure.AssemblyInfo.cs
+++ ExpenseTracker.Infrastructure/obj/Debug/net9.0/ExpenseTracker.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+336b789795104f354fe72320fdeb1991f8f3600a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e3d06b08c8fa3ee0f87d77f73810b37715650a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
