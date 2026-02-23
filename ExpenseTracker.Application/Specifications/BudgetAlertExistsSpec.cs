6fb2006b256498fd9ed72ac819ff0c3dc74b03d2
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-02-23 15:35:29 +0400

    added specification that checks if budget alert exists

---
+++ ExpenseTracker.Application/Specifications/BudgetAlertExistsSpec.cs
@@ -0,0 +1,25 @@
+﻿using ExpenseTracker.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Application.Specifications;
+
+public class BudgetAlertExistsSpec : BaseSpecification<BudgetAlert>
+{
+    // summary of the constructor: Initializes a new instance of the BudgetAlertExistsSpec class with the specified user ID, category ID, year, and month. It builds the criteria for checking if a budget alert exists based on these parameters.
+    public BudgetAlertExistsSpec(
+        Guid userId,
+        Guid categoryId,
+        int year,
+        int month)
+        : base(a =>
+            a.UserId == userId &&
+            a.CategoryId == categoryId &&
+            a.Year == year &&
+            a.Month == month)
+    {
+    }
+}
--- ExpenseTracker.Application/obj/Debug/net9.0/ExpenseTracker.Application.AssemblyInfo.cs
+++ ExpenseTracker.Application/obj/Debug/net9.0/ExpenseTracker.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bc9589531aa3099ea9ec4ef37d0eaabeb56d89d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5294ecfa882185543a99a191bfd2d7c46d6ad93")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
