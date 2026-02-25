4d7ff7d68f7106e6258f7bde0f6b2e5f08fe9b3c
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-02-25 12:42:10 +0400

    added reportjob controller

---
+++ ExpenseTrackerApi/Controllers/ReportController.cs
@@ -0,0 +1,29 @@
+﻿using ExpenseTracker.Application.DTOs.Reports;
+using ExpenseTracker.Application.Interfaces.Reports;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTrackerApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ReportController : ControllerBase
+{
+    public readonly IReportService _reportService;
+
+    public ReportController(IReportService reportService)
+    {
+        _reportService = reportService;
+    }
+
+    [HttpPost("monthly")]
+    public async Task<IActionResult> CreateMonthlyReport([FromBody] CreateMonthlyReportRequest request)
+    {
+        var reportId = await _reportService.CreateMonthlyReportAsync(request);
+        return Accepted(new
+        {
+            reportId,
+            status = "pending"
+        });
+    }
+}
--- ExpenseTrackerApi/obj/Debug/net9.0/ExpenseTrackerApi.AssemblyInfo.cs
+++ ExpenseTrackerApi/obj/Debug/net9.0/ExpenseTrackerApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTrackerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14080fb5da26f867658757b6082c5b93c8ce3ea1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2aa20f2e1fabf8f61d217ff7cdde496322ffd581")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTrackerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTrackerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
