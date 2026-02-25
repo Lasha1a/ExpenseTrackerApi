9258729290a5bdb26a6865bf5ac3deb2a0f3ff70
lasha tinikashvili <lashatinikashvili29@gmail.com>
2026-02-25 12:22:18 +0400

    added reportjobs dto and interface

---
+++ ExpenseTracker.Application/Interfaces/Reports/IReportService.cs
@@ -0,0 +1,13 @@
+﻿using ExpenseTracker.Application.DTOs.Reports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Application.Interfaces.Reports;
+
+public interface IReportService
+{
+    Task<Guid> CreateMonthlyReportAsync(CreateMonthlyReportRequest request);
+}
--- ExpenseTrackerApi/obj/Debug/net9.0/ExpenseTrackerApi.AssemblyInfo.cs
+++ ExpenseTrackerApi/obj/Debug/net9.0/ExpenseTrackerApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTrackerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5b5c0829802ac03ac322bec522ef9ffb80f04e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14080fb5da26f867658757b6082c5b93c8ce3ea1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTrackerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTrackerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
